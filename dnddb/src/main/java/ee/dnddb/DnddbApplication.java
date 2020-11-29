dda2eab276783cfbb111a4ba04bb556940d12c7b
Andreas Tali <taliandreas@gmail.com>
2020-11-29 12:47:24 +0200

    first commit

---
+++ dnddb/src/main/java/ee/dnddb/DnddbApplication.java
@@ -0,0 +1,13 @@
+package ee.dnddb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnddbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnddbApplication.class, args);
+	}
+
+}
---
+++ dnddb/src/test/java/ee/dnddb/DnddbApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.dnddb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnddbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
